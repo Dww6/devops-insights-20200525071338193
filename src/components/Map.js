157b6a314ac13f63cd6ac26e765c3d74ec33d863
Daniel Wilson <danintendone@gmail.com>
2020-05-31 23:18:03 +0000

    Fixed error where the what was returned from the map component was not in brackets

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,17 +1,16 @@
 import React from 'react';
 
 function Map() {
-	<div id="map"></div>
-	<script>
+	return (<div id="map"></div>
 	//First sets up the map to be at a default location
-	function initMap() {
-	  var location = {lat: -37.78, lng: 175.28};
-	  var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: location});
-	}
+	<script>
+	  function initMap() {
+	    var location = {lat: -37.78, lng: 175.28};
+   	    var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: location});
+	  }
 	</script>
-    return <script async defer
-  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAaYw3FQdv6bXgjgzbDlKfX1WeolDTkGGg&callback=initMap">
-</script>;
+ 	<script async defer
+  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAaYw3FQdv6bXgjgzbDlKfX1WeolDTkGGg&callback=initMap">;);
 }
   
 export default Map
