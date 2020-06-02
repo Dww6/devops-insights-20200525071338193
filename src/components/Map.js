8a817c6a564831900eff9c74222a599fa6c6829b
Daniel Wilson <danintendone@gmail.com>
2020-06-02 08:03:47 +0000

    Removed script from index.html to put inside the Map.js file

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,6 +3,13 @@ import React from 'react';
 function Map() {
 	return(
 	    <div id="map"></div>
+	    <script>
+    	  function initMap() {
+    	    var location = {lat: -37.78, lng: 175.28};
+       	  var map = new google.maps.Map(document.getElementById('map'), {zoom: 14, center: location});
+    	  }
+    	</script>
+     	<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBSCFLkh3OK6sF0Mfo3h_89K_aFrkS58wA&callback=initMap"></script>
 	);
 }
   
