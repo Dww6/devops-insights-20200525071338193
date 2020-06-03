d9a194177e5f5ad4fba7f73be8be77d5d2b37331
Daniel Wilson <danintendone@gmail.com>
2020-06-03 04:39:27 +0000

    Add text between div

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 function Map() {
-	let location = document.createElement('div');
-	location.innerHTML = "<div id=\"map\"></div> <script> function initMap() {location; map;}</script><script async defer src=\"https://maps.googleapis.com/maps/api/js?key=AIzaSyBSCFLkh3OK6sF0Mfo3h_89K_aFrkS58wA&callback=initMap\"></script>";
+	//let location = document.createElement('div');
+	//location.innerHTML = "<div id=\"map\"></div> <script> function initMap() {location; map;}</script><script async defer src=\"https://maps.googleapis.com/maps/api/js?key=AIzaSyBSCFLkh3OK6sF0Mfo3h_89K_aFrkS58wA&callback=initMap\"></script>";
 	return(
-		location;
+			<div>"<div id=\"map\"></div> <script> function initMap() {location; map;}</script><script async defer src=\"https://maps.googleapis.com/maps/api/js?key=AIzaSyBSCFLkh3OK6sF0Mfo3h_89K_aFrkS58wA&callback=initMap\"></script>"</div>
 	);
 }
   
